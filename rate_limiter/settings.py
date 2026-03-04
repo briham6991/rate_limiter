c680ea7e5425a633a0b8ddf114b6631b7f83c801
briham6991 <pbriham@gmail.com>
2026-03-04 23:12:16 +0530

    modified settings to include postgres sql database settings from environ and other settings info saved in .env file

--- rate_limiter/settings.py
+++ rate_limiter/settings.py
@@ -8,24 +8,34 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
-"""
+""" 
 
+import environ
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = env.bool('DEBUG', default=False) # DEBUG value from .env file, default to False if not set
+DATABASES = {
+    'default': env.db('DATABASE_URL')
+}
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nl%q-$w+chck&3ctm#rz8&yyj7sl*8ss%qd-lp$j3!b)_3h+d2'
+# SECRET_KEY = 'django-insecure-nl%q-$w+chck&3ctm#rz8&yyj7sl*8ss%qd-lp$j3!b)_3h+d2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=["127.0.0.1"]) # ALLOWED_HOSTS value from .env file, default to empty list if not set
 
 
 # Application definition
@@ -37,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -72,12 +83,12 @@ WSGI_APPLICATION = 'rate_limiter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
@@ -115,3 +126,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+AUTH_USER_MODEL = 'core.CustomUser'
