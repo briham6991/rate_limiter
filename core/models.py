eccc6a4e5e0d1845dcf8677ffc586f5193a89569
briham6991 <pbriham@gmail.com>
2026-03-12 12:02:02 +0530

    Implement DELETE /keys/{key_id} endpoint with permission check - closes #1

--- core/models.py
+++ core/models.py
@@ -2,23 +2,45 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+class CompanyInformation(models.Model):
+    company_id = models.BigAutoField(primary_key=True)
+    company_name = models.CharField(max_length=250)
+    email_id = models.EmailField(unique=True) # check for argumrnt to put in EmailValidator
+    contact_number = models.CharField(max_length=15, unique=True)
+    address = models.TextField()
+    join_date = models.DateTimeField(null=True, blank=True)
+    is_company_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.company_name
+
+    class Meta:
+        db_table = 'company_information'
+        verbose_name = 'Company Information'
+
+
+
 class CustomUser(AbstractUser):
     user_id = models.BigAutoField(primary_key=True)
     phone_number = models.CharField(max_length=10, unique=True)
     role = models.SmallIntegerField(default=2, db_comment="(1=superadmin, 2=admin, 3=readonly)")
+    company_id = models.ForeignKey(CompanyInformation, on_delete=models.PROTECT, null=True, blank=True, db_column='company_id')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     employee_id = models.CharField(max_length=50, unique=True, null=True, blank=True)
 
     def __str__(self):
         return self.username
-  
 
     class Meta:
         db_table = 'custom_user'
-        verbose_name = 'Custom User'    
+        verbose_name = 'Custom User'
 
-
+    def clean(self):
+        pass # TODO: Implement the validation that whenever superadmin is created company_id should be null and whenever admin or readonly user is created company_id should not be null and also check for the role value and other things that are important to maintain the integrity of the data in the database. This clean method will be called before saving the user instance to the database and if any validation error occurs it will raise a ValidationError which can be handled in the views and appropriate response can be sent to the client.
+         
 
 class PlanInformation(models.Model): 
     plan_id = models.BigAutoField(primary_key=True)
@@ -40,24 +62,6 @@ class PlanInformation(models.Model):
         verbose_name = 'Plan Information'
 
 
-class CompanyInformation(models.Model):
-    company_id = models.BigAutoField(primary_key=True)
-    company_name = models.CharField(max_length=250)
-    email_id = models.EmailField(unique=True) # check for argumrnt to put in EmailValidator
-    contact_number = models.CharField(max_length=15, unique=True)
-    address = models.TextField()
-    join_date = models.DateTimeField(null=True, blank=True)
-    is_company_active = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.company_name
-
-    class Meta:
-        db_table = 'company_information'
-        verbose_name = 'Company Information'
-
 
 class KeyInformation(models.Model):
     key_id = models.BigAutoField(primary_key=True)
@@ -78,6 +82,7 @@ class KeyInformation(models.Model):
     class Meta:
         db_table = 'key_information'
         verbose_name = 'Key Information'
+
 
 
 class KeyUtilizationInfo(models.Model):
--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,8 @@ from core import views
 
 urlpatterns = [
     path('key/', views.CreateAPIKeyView.as_view(), name="APIkey"),
+    path('key/<int:pk>/', views.DeleteAPIKeyView.as_view(), name="delete_api_key"),
+
 
 
 ]
