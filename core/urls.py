44628852f86d96fc03daf5434234dd3cfb1dd2ae
briham6991 <pbriham@gmail.com>
2026-03-18 16:57:58 +0530

    Implement GET /auth/validate endpoint - closes #2

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,7 @@ from core import views
 urlpatterns = [
     path('key/', views.CreateAPIKeyView.as_view(), name="APIkey"),
     path('key/<int:pk>/', views.DeleteAPIKeyView.as_view(), name="delete_api_key"),
+    path('access-validate/', views.GetUserDetailsFromTokenView.as_view(), name="access_validation"),
 
 
 
